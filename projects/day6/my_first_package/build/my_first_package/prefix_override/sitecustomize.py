5e585379bef64d28c02eaf8d02ff5dc5e2c2cbc5
Jashu0718 <ejashwanthreddy2002@gmail.com>
2026-04-20 14:22:05 +0000

    Day 6 - First ROS2 package with talker and listener nodes

---
+++ projects/day6/my_first_package/build/my_first_package/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/jashdynamics/robotics-sprint-2026/projects/day6/my_first_package/install/my_first_package'
